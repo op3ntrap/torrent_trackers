d9071994d0fa913e8abceb2720c4c5daef2b3ae5
op3ntrap <op3ntrap@gmail.com>
2020-09-28 07:06:53 +0530

    :)

---
+++ newtrackon.py
@@ -0,0 +1,68 @@
+import requests
+
+"""
+NewTrackon Web API
+<https://newtrackon.com/api>
+
+GET https://newtrackon.com/api/stable
+Returns a two line delimited list announce URLs of trackers that have an uptime
+of equal or more than 95%.
+
+GET https://newtrackon.com/api/:percentage
+Returns a two line delimited list announce URLs of trackers that have an uptime
+of equal or more than the given percentage.
+
+GET https://newtrackon.com/api/all
+Returns a two line delimited list announce URLs of all known trackers, dead or
+alive.
+
+GET https://newtrackon.com/api/live
+Returns a two line delimited list announce URLs of currently active and
+responding trackers.
+
+GET https://newtrackon.com/api/udp
+Returns a two line delimited list announce URLs of stable and UDP trackers.
+
+GET https://newtrackon.com/api/http
+Returns a two line delimited list announce URLs of stable and HTTP/HTTPS
+trackers.
+"""
+
+
+class NewTrackon(fandom):
+    """docstring for .NewTrackon"""
+
+    def __init__(self, *args, **kwargs):
+        self.base_url = "https://newtrackon.com/api/"
+        self.live = self.base_url + "live"
+        self.udp = self.base_url + "udp"
+        self.http = self.base_url + "http"
+        # Data Accumulation
+        self.stable_data = [
+            link
+            for link in requests.get(self.base_url + stable).text.split("\n\n")
+            if len(link) > 2
+        ]
+        self.live_data = [
+            link for link in requests.get(self.live).text.split("\n\n") if len(link) > 2
+        ]
+        self.udp_data = [
+            link for link in requests.get(self.udp).text.split("\n\n") if len(link) > 2
+        ]
+        self.http_data = [
+            link for link in requests.get(self.http).text.split("\n\n") if len(link) > 2
+        ]
+        # Data Filtering
+        self.udp_live_data = [link for link in self.udp_data if link in self.live_data]
+        self.http_live_data = [
+            link for link in self.http_data if link in self.live_data
+        ]
+
+    def trackers(self):
+        self.master_dump = self.stable_data + self.udp_live_data + self.http_live_data
+        # Add all the remaining trackers to list from the live data if they all
+        # are not already there
+        for remaining_tracker in self.live_data:
+            if remaining_tracker not in self.master_dump:
+                if len(remaining_tracker) > 2:
+                    self.master_dump.append(remaining_tracker)
